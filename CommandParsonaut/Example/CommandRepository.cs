dec44de5d953ab54c31ada0a8d1fbf0e9f86e185
Marek Eibel <536376@mail.muni.cz>
2024-05-08 22:19:18 +0200

    feat: add example

---
+++ CommandParsonaut/Example/CommandRepository.cs
@@ -0,0 +1,23 @@
+﻿using CommandParsonaut.Core.Types;
+
+namespace RSSFeedifyCLIClient.Repository
+{
+    /// <summary>
+    /// Stores all commands in the map.
+    /// </summary>
+    public static class CommandsRepository
+    {
+        public static Dictionary<string, Command> InitCommands()
+        {
+            Dictionary<string, Command> commands = new Dictionary<string, Command>();
+
+            Command sum = new Command("sum", "a:INTEGER b:INTEGER c:INTEGER", "Sums three integers (i = 0 to 2) in range <100 * i, 100 * (i + 1) - 1>.", new List<ParameterType> { ParameterType.IntegerRange, ParameterType.IntegerRange, ParameterType.IntegerRange, }, new List<(int, int)> { (0, 99), (100, 199), (200, 299) });
+            Command quit = new Command("quit", "", "Quits the application.", new List<ParameterType> { });
+
+            commands["sum"] = sum;
+            commands["quit"] = quit;
+
+            return commands;
+        }
+    }
+}
---
+++ CommandParsonaut/Example/Program.cs
@@ -0,0 +1,59 @@
+﻿using CommandParsonaut.CommandHewAwayTool;
+using CommandParsonaut.Core.Types;
+using CommandParsonaut.Interfaces;
+using RSSFeedifyCLIClient.IO;
+using RSSFeedifyCLIClient.Repository;
+
+namespace RSSFeedifyCLIClient
+{
+    public class Application
+    {
+        /// <summary>
+        /// Basic example on how to use CommandParsonaut class library.
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            var commands = CommandsRepository.InitCommands();
+            IWriter writer = new Writer();
+            IReader reader = new Reader();
+
+            var parser = new CommandParser(writer, reader);
+            parser.AddCommands(commands.Values.ToList());
+
+            bool appRunning = true;
+            while (appRunning)
+            {
+                Command receivedCommand;
+                IList<ParameterResult> parameters;
+                string unprocessedInput;
+                if (parser.GetCommand(out receivedCommand, out parameters, out unprocessedInput))
+                {
+                    switch (receivedCommand.Name)
+                    {
+                        case "quit":
+                            appRunning = false;
+                            break;
+                        case "sum":
+                            writer.RenderBareText($"Entered command: <{unprocessedInput}>");
+                            writer.RenderBareText($"Sum: {HandleSumCommand(parameters)}");
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+
+        private static int HandleSumCommand(IList<ParameterResult> parameters)
+        {
+            int sum = 0;
+            foreach (ParameterResult parameterResult in parameters)
+            {
+                sum += parameterResult.Integer;
+            }
+
+            return sum;
+        }
+    }
+}
